209365d4ffa374229a62562acc9fd1645e946b46
Fokhrul Islam <shahidcse12@gmail.com>
2022-05-19 04:22:52 +0600

    database integration

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,35 +1,51 @@
 import React, {} from 'react';
 import './ToDoList.css'
+import { useForm } from 'react-hook-form';
 
 const ToDoList = () => {
 
-    const handleSubmit =  (e) => {
-        e.preventDefault();
-
-        const task = e.target.task.value;
-        const description = e.target.description.value;
-        console.log(task , description);
-        const url = 'http://localhost:5000/todo';
+    const { register,  handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        const url = 'http://localhost:5000/tasks';
         fetch(url,{
             method:'POST',
             headers:{
                 'content-type':'application/json'
             },
-            body:JSON.stringify(task)
+            body:JSON.stringify(data)
         })
         .then(res=>res.json())
-        
-
-        e.target.reset(' ')
+        .then(result=>{
+            const url = 'http://localhost:5000/todo';
+           fetch (url)
+           .then(res=>res.json())
+           .then(result=>{})
+           
+           
+        })
+  
     }
         return (
             <div className='todo '>
                 <h2 className='text-primary text-center mt-5 mb-3'>What to Do... </h2>
                 
-                <form onSubmit={handleSubmit} >
-                    <input type="text" name="task" id="" placeholder='Task Name' required />
+                <form onSubmit={handleSubmit(onSubmit)} >
+                    <input type="text" name="task" id="" placeholder='Task Name' required 
+                    {...register("task", {
+                        required: {
+                            value: true,
+                            message: 'Task is Required'
+                        }
+                    })} />
 
-                    <input type="textarea" name="description" id="" placeholder='Enter what to do' required />
+                    <input type="textarea" name="description" id="" placeholder='Enter what to do' required 
+                    {...register("description", {
+                        required: {
+                            value: true,
+                            message: 'Description is Required'
+                        }
+                    })}/>
 
                     <input
 
